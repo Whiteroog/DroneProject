dbf01bf5e280d04aa2f84ee2d27a99c2e112710a
Nikita <Whiteroog@gmail.com>
2022-07-26 11:20:28 +0300

    Fix interp methods and clear

--- Source/DroneProject/Public/Drone/DroneMovementComponent.h
+++ Source/DroneProject/Public/Drone/DroneMovementComponent.h
@@ -38,13 +38,13 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Drone movement | Reflection", meta = (ClampMin = 100.0f, UIMin = 100.0f, ClampMax = 1000.0f, UIMax = 1000.0f))
 	float MaxSpeedForReflection = 400.0f;
 
-	// отдельная настройка скорости поворота дрона (Не камеры)
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Drone | Tilt", meta = (ClampMin = 10.0f, UIMin = 10.0f, ClampMax = 300.0f, UIMax = 300.0f))
-	float SpeedRotationYaw = 50.0f;
-
-	// отдельная настройка скорости Наклона дрона (Не камеры)
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Drone | Tilt", meta = (ClampMin = 10.0f, UIMin = 10.0f, ClampMax = 100.0f, UIMax = 100.0f))
-	float SpeedTurn = 50.0f;
+	// // отдельная настройка скорости поворота дрона (Не камеры)
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Drone | Tilt", meta = (ClampMin = 10.0f, UIMin = 10.0f, ClampMax = 300.0f, UIMax = 300.0f))
+	// float SpeedRotationYaw = 50.0f;
+	//
+	// // отдельная настройка скорости Наклона дрона (Не камеры)
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Drone | Tilt", meta = (ClampMin = 10.0f, UIMin = 10.0f, ClampMax = 100.0f, UIMax = 100.0f))
+	// float SpeedTurn = 50.0f;
 
 	// ускорение наклона
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Drone | Tilt", meta = (ClampMin = 1.0f, UIMin = 1.0f, ClampMax = 10.0f, UIMax = 10.0f))
@@ -68,18 +68,18 @@ private:
 	// шаблонный метод передвижения со скольжение вдоль стены
 	void MoveComponent(float DeltaTime, FVector Delta, const FRotator NewRotation);
 
-	// плавное обновление скорости (проблема - медленно доходит до максимума и минимума)
+	// плавное обновление скорости
 	void SetVelocityByInterp(float DeltaTime, const FVector InputVector);
-	
-	// линейное обновление скорости (проблема - на концах ограничения дрон не может из-за большой дельты встать в предел)
-	void SetVelocityByClamp(float DeltaTime, const FVector InputVector);
-
-	// константа хранящая текущий угол поворота дрона и нулевые координаты
-	FRotator GetParallelGroundRotation() const;
 
 	// плавный наклон дрона
 	FRotator DroneTiltByInterp(float DeltaTime, const FVector InputVector) const;
 
-	// линейный наклон дрона (проблема - не привязанный дрон к контроллеру по оси Y, дрожит при движении из-зи большой дельты между контроллером и дроном)
-	FRotator DroneTiltByClamp(float DeltaTime, const FVector InputVector) const;
+	// константа хранящая текущий угол поворота дрона и нулевые координаты
+	FRotator GetParallelGroundRotation() const;
+	
+	// // линейное обновление скорости (проблема - на концах ограничения дрон не может из-за большой дельты встать в предел)
+	// void SetVelocityByClamp(float DeltaTime, const FVector InputVector);
+	//
+	// // линейный наклон дрона (проблема - не привязанный дрон к контроллеру по оси Y, дрожит при движении из-зи большой дельты между контроллером и дроном)
+	// FRotator DroneTiltByClamp(float DeltaTime, const FVector InputVector) const;
 };
